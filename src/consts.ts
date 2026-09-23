6fff4bf9b6b9069ee059e546d3d6c7f4c3049c86
河合毅 <kawaitakeshi@MacBookAir.AirPort>
2026-09-23 21:43:00 +0900

    サイト名を変更

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'モノログ（MonoLog）';
+export const SITE_DESCRIPTION = 'おすすめのガジェットや買ってよかった日用品を紹介するレビューブログ';
